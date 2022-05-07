2eecfdcde2e48cada93b7603732c325baa57f30c
sg-nta <an.nt204865@sis.hust.edu.vn>
2022-05-07 21:52:34 +0700

    Refactor the Cart class

--- Lab03/AimsProject/src/Aims.java
+++ Lab03/AimsProject/src/Aims.java
@@ -2,6 +2,7 @@
 public class Aims {
 	public static void main(String[] args) {
 		Cart anOrder = new Cart();
+		
 		//Test method create DVD and method addDigitalVideoDisc
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
 				"Animation","Roger Allers", 87, 19.95f);
@@ -13,23 +14,29 @@ public class Aims {
 				"Animation", 18.99f);
 		anOrder.addDigitalVideoDisc(dvd3);
 		System.out.println("Total Cost is: " + anOrder.totalCost());
+		DigitalVideoDisc dvd4 = new DigitalVideoDisc("XYZ",
+				"Animation", 40f);
+		anOrder.addDigitalVideoDisc(dvd4);
+		System.out.println("Total Cost is: " + anOrder.totalCost());
 		
 		//Test method removeDigitalVideoDisc
 		anOrder.removeDigitalVideoDisc(dvd2);
 		System.out.println("Total Cost is: " + anOrder.totalCost());
-		DigitalVideoDisc dvd4 = new DigitalVideoDisc("XYZ",
-				"Animation", 200f);
-		anOrder.addDigitalVideoDisc(dvd4);
-		System.out.println("Total Cost is: " + anOrder.totalCost());
+		anOrder.removeDigitalVideoDisc(dvd2);
+
 		
 		//Test method addDigitalVideoDisc for list of discs
+		
 		DigitalVideoDisc[] discArr = new DigitalVideoDisc[4];
-		discArr[0] = dvd1;
-		discArr[1] = dvd2;
-		discArr[2] = dvd3;
-		discArr[3] = dvd4;
+		discArr[0] = new DigitalVideoDisc("Science fact","Science",20f);
+		discArr[1] = new DigitalVideoDisc("Biology study","Biology",40f);
+		discArr[2] = new DigitalVideoDisc("Math study","Math",25f);
+		discArr[3] = new DigitalVideoDisc("Python tutorials","Programming",60f);
 		anOrder.addDigitalVideoDisc(discArr);
 		System.out.println("Total Cost is: " + anOrder.totalCost());
-	
+		
+
+
+		
 	}
 }
